e04b4d105a2645378e3ebf9834b813e04826a186
Ajaya Prajapati <ajz.prz@gmail.com>
2024-06-20 11:47:48 -0400

    first commit

---
+++ models/Item.js
@@ -0,0 +1,30 @@
+// models/Item.js
+const mongoose = require('mongoose');
+
+const itemSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Item = mongoose.model('Item', itemSchema);
+
+module.exports = Item;
